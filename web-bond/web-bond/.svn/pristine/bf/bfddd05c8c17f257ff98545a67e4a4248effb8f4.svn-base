10e6c76ac35cf2af330e54634958ffdc37a53d23
yali_chen <yali_chen@systex.com.cn>
2018-11-22 10:36:23 +0800

    1

--- web-bond/web-bond/.svn/pristine/bf/bfddd05c8c17f257ff98545a67e4a4248effb8f4.svn-base
+++
@@ -1,8 +0,0 @@
-// import { combineReducers } from 'redux';
-// import loginList from './loginList';
-
-// const login = combineReducers({
-//   loginList
-// });
-
-// export default login;
--- web-bond/web-bond/src/redux/reducers/register/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import indexReginster from './indexReginster';
-
-const reginster = combineReducers({
-    indexReginster
-});
-
-export default reginster;
